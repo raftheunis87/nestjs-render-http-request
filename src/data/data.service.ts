237432a7496941e75d7482c802380242e3f29b13
Jay McDoniel <jmcdo29@gmail.com>
2019-09-30 22:42:58 -0700

    feat(filter): add BadRequestFilter to catch errors and render them

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, Logger, HttpService } from '@nestjs/common';
-import { map, tap } from 'rxjs/operators';
+import { Injectable, Logger, HttpService, BadRequestException } from '@nestjs/common';
+import { map, tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
@@ -9,10 +9,17 @@ export class DataService {
 
     getData() {
         return this.httpService.get('https://jsonplaceholder.typicode.com/todos/1').pipe(
+            tap(() => {
+                throw new Error();
+            }),
             map(response => response.data),
             map(resData => ({
-                message: resData.title
-            }))
-        )
+                message: resData.title,
+            })),
+            catchError((err: any) => {
+                this.logger.error(err);
+                throw new BadRequestException();
+            }),
+        );
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestFilter } from './bad-request.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,7 +12,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-
+  app.useGlobalFilters(new BadRequestFilter());
   await app.listen(3000);
 }
 bootstrap();
